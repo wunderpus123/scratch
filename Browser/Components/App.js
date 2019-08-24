a363139db6fcf3bda1390afc1f76987135ee4574
Natia Khomasuridze <natiakhoma@gmail.com>
2019-08-24 09:54:10 -0400

    file structure for FE

---
+++ Browser/Components/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+const App = () => {
+    
+    return(
+        <div>
+            
+            <div>
+                //render components here
+            </div>
+        
+        </div>
+    )
+}
+export default App
