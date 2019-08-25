eb3630be5a5111f70ef9e571fc95c43d5925ea39
xak200 <xak200@nyu.edu>
2019-08-25 14:56:32 -0400

    cleanup again

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
-import store from "./store";
+import store from "./redux/store";
 
 render(
   // wrap the App in the Provider and pass in the store
